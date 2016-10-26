2ae9a8818c681a1b8b2f68e39b9b5f5204af0502
Megan Bruening <mbruening18@my.whitworth.edu>
2016-10-26 12:32:57 -0700

    start

---
+++ CS172-HW05/CS172-HW05/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  CS172-HW05
+//
+//  Created by Megan Bruening on 10/26/16.
+//  Copyright © 2016 Megan Bruening. All rights reserved.
+//
+//EX05_01 – Liang Programming Exercise 12.2: Linear Search
+//EX05_02 – Liang Programming Exercise 12.4: Is sorted?
+//EX05_03 – Liang Programming Exercise 12.8: Implement vector class
+//EX05_04 – Liang Programming Exercise 12.20: Shuffle vector
+//EX05_05 – Liang Programming Exercise 12.25: new Account class
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+    
+}
